db0ba437b9f6f27e7baaeb370e2b50e9509f8604
Acer <Acer@LAPTOP-341IHVIP>
2023-11-28 22:51:10 +0200

    Add project files.

---
+++ oop lab 9/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Коляда Олександра
+using System;
+// Базовий клас
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Animal makes a sound.");
+    }
+}
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Dog barks.");
+    }
+}
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Cat meows.");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Animal myAnimal = new Animal();
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+        // Виклики методів з використанням поліморфізму
+        myAnimal.MakeSound();
+        myDog.MakeSound();
+        myCat.MakeSound();
+    }
+}
